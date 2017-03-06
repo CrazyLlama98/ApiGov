3ffe9cd0ecd6cb460c3029402c446b3724d81b0f
liviu <liviu@DESKTOP-I55MJ32>
2017-03-06 19:43:16 +0200

    Final API Hackathon project

---
+++ APIGovApp/dbo.Authenticator.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[AccessToken] VARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Username] VARCHAR(50) NOT NULL, 
+    [Password] VARCHAR(50) NOT NULL, 
+    [PermissionType] INT NOT NULL
+)
